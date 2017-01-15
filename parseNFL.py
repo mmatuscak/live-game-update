26ac413c32a22f5eea781b50524def933f3526fb
matt <mmatu21@gmail.com>
2017-01-15 15:26:57 -0500

    Changed url to postseason xml

--- parseNFL.py
+++ parseNFL.py
@@ -1,35 +1,38 @@
 import requests
-import json
+from lxml import etree
 from termcolor import colored
 
-NFL_URL = "http://www.nfl.com/liveupdate/scorestrip/ss.json"
+#NFL_URL = "http://www.nfl.com/liveupdate/scorestrip/ss.xml"
+NFL_URL = "http://www.nfl.com/liveupdate/scorestrip/postseason/ss.xml"
 
 def print_week(week, games):
     """Prints Scores. VISITOR: ##  HOME: ##""" 
 
-    print(colored("-------- WEEK {} --------".format(week), 'white'))
-    print(colored("VISITOR", 'red'),'\t', colored("HOME", 'blue'))
+    #print(colored("-------- WEEK {} --------".format(week), 'white'))
+    print(colored("-------- Postseason --------".format(week), 'white'))
+    #print(colored("VISITOR", 'red'),'\t', colored("HOME", 'blue'))
 
-    day_prev = games[0]['d']
+    day_prev = games[0].get('d')
     print(colored("{}".format(day_prev), 'white'))
 
     for game in games:
-        day = game['d']
-        time = game['t'] # game start time, EST
-        home = game['h']
-        visitor = game['v']
-        h_score = game['hs']
-        v_score = game['vs']
+        day = game.get('d')
+        time = game.get('t') # game start time, EST
+        home = game.get('h')
+        visitor = game.get('v')
+        h_score = game.get('hs')
+        v_score = game.get('vs')
         if day_prev != day:
             print(colored("{}".format(day), 'white'))
-        print("{} - {:<4s}:{:>3d}\t{:<4s}:{:>3d}".format(time, visitor, v_score, home, h_score))
+        #print("{} - {:<4d}:{:>3d}\t{:<4d}:{:>3d}".format(time, visitor, v_score, home, h_score))
+        print("{} - {}:{} \t@\t{}:{}".format(time, visitor, v_score, home, h_score))
         day_prev = day
 
 def get_json():
     r = requests.get(NFL_URL)
-    j = json.dumps(r.json())
-    j_data = json.loads(j)
-    week = j_data['w']
-    games = j_data['gms']
+    xml = etree.XML(r.content)
+    games = xml.find('gms')
+    week = games.get('w')
+    games = [g for g in games.findall('g')] 
 
     return week, games
